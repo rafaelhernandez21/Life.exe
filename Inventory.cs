c500ed1a5c18f7e0936a01f5a33973eaba8c9f18
rafaelhernandez21 <43394316+rafaelhernandez21@users.noreply.github.com>
2019-03-13 21:37:32 -0500

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LifeTest
+{
+    public class Inventory
+    {
+        public List<Item> Items = new List<Item>();
+
+        public int maxWeight;
+        public int currentWeight;
+
+        void AddItem(Item i)
+        {
+            if (Items.Contains(i)) {
+                Console.WriteLine("You already have this item");
+            }
+            else {
+                Items.Add(i);
+            }
+
+        }
+        void RemoveItem(Item i) { 
+            if (Items.Contains(i)) {
+                Items.Remove(i);
+
+            }
+            else {
+                Console.WriteLine("You don't even have this item!");
+            }
+        }
+        public Inventory()
+        {
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LifeTest
+{
+    public abstract class Item
+    {
+        public string name;
+        public int weight;
+        public int value;
+        public enum ItemType { food, tool, armor, misc};
+        public ItemType itemType;
+
+       
+    }
+
+
+}
