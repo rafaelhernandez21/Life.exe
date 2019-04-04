ca214cf7863be240c514542bc916a146bb6eab1c
rafaelhernandez21 <43394316+rafaelhernandez21@users.noreply.github.com>
2019-04-04 15:28:42 -0500

    Update Trade.cs

--- Assets/Scripts/Demo/Trade.cs
+++ Assets/Scripts/Demo/Trade.cs
@@ -7,10 +7,25 @@ namespace LifeTest{
 //bad trades lower reputations
 //giving a person something poisonous or dangerous can increase fear, adds to clout 
 //good trades increase trust
-public class Trade {
-  public void VerifyTrade(Item i1, Item i2){
+  public class Trade {
+    public void VerifyTrade(Item i1, Item i2){
+      if(i1.owner == i2.owner || i1 == i2){
+        
+        Print("Invalid Trade");
+      }else{
+        InitiateTrade(i1.owner, i1, i2.owner, i2);
+      }
+    }
+    public void InitiateTrade(Character owner1, Item item1, Character owner2, Item item2){
+      if(item1.value == item2.value){
+        //decent trade
+      }else if (item1.value < item2.value){
+      //good trade 
+      }else if (item1.value > item2.value){
+      //bad trade
+      }
     
+    }
   }
-}
 
 }
