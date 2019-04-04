8d1b9bd3b527fd2c920bdddaf6aa78f9d9e9d3dc
rafaelhernandez21 <43394316+rafaelhernandez21@users.noreply.github.com>
2019-04-03 21:28:50 -0500

    Create Trade.cs

---
+++ Assets/Scripts/Demo/Trade.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LifeTest{
+//essentially just make a trade interface where items from each inventory can be selected and traded
+//bad trades lower reputations
+//giving a person something poisonous or dangerous can increase fear, adds to clout 
+//good trades increase trust
+
+
+}
